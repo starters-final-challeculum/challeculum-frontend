cc6471dbec7e48ab35ae9a13026f8f28149b3d13
sydneyyyyyyy <qwer1478792@gmail.com>
2023-02-23 16:36:45 +0900

    fix: 이미 등록한 강의는 목록에서 제외되도록 구현

---
+++ src/pages/private/AdminPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { withLayout } from '../../components/common/layout/Layout';
+
+function AdminPage() {
+  return (
+    <>
+      관리자
+    </>
+
+  );
+}
+export default withLayout(AdminPage);
