4630ae4beef230fc8c54fefbd16cf83e76c900f3
sydneyyyyyyy <qwer1478792@gmail.com>
2023-03-02 03:16:31 +0900

    design: 메인페이지 화면 수정

--- src/components/private/adminPage/MissionTab.jsx
+++ src/components/private/adminPage/MissionTab.jsx
@@ -3,12 +3,11 @@ import tw from 'tailwind-styled-components';
 import api from '../../../common/axios-config';
 
 function MissionTab() {
-  const [mission, setMission] = useState('');
+  const [mission, setMission] = useState([]);
 
   const getMission = async () => {
     api.get('user/me/all').then((response) => {
       setMission(response.data);
-      console.log(response.data);
     });
   };
 
@@ -17,89 +16,105 @@ function MissionTab() {
   }, []);
 
   const onUpdateHandler = async (event) => {
-    api.put(`/user/me/mission/${event.target.id}`, {
+    const missionId = event.target.id;
+    const missionToUpdate = mission.find((item) => item.missionId === missionId);
+    console.log(missionToUpdate);
 
-    }).then((response) => {
-      console.log(response);
-      alert('수정완료');
-      getMission();
-    });
+    api.put(`/user/me/mission/${missionId}`, {
+      submitAt: missionToUpdate.submitAt,
+      isAccepted: 'ACCEPTED',
+      imageUrl: missionToUpdate.imageUrl,
+      userId: missionToUpdate.userId,
+    })
+      .then((response) => {
+        console.log(response);
+        alert('수정완료');
+        getMission();
+      });
   };
 
-  // const updateInfo = () => {
-  //   api.patch('/user', {
-  //     nickname: updateNickname.current.value,
-  //     phone: updatePhone.current.value,
-  //     password: updatePassword.current.value,
-  //   }).then((response) => {
-  //     setInfo({
-  //       ...response.data,
-  //       username: info.username,
-  //     });
-  //     updateNickname.current.value = response.data.nickname;
-  //     updatePhone.current.value = response.data.phone;
-  //     updatePassword.current.value = response.data.password;
-  //     alert('수정완료');
-  //     setUpdateMode(false);
-  //     window.location.reload();
-  //   });
-  // };
+  const onRejectHandler = async (event) => {
+    const missionId = event.target.id;
+    const missionToUpdate = mission.find((item) => item.missionId === missionId);
+    console.log(missionToUpdate);
+
+    api.put(`/user/me/mission/${missionId}`, {
+      submitAt: missionToUpdate.submitAt,
+      isAccepted: 'REJECTED',
+      imageUrl: missionToUpdate.imageUrl,
+      userId: missionToUpdate.userId,
+    })
+      .then((response) => {
+        console.log(response);
+        alert('수정완료');
+        getMission();
+      });
+  };
 
   return (
     <ListContainer>
-      {mission && mission.map((item) => (
-        <ListCard key={item.missionId} className="grid grid-cols-6">
-          <FirstBox>
-            <div className="text-lg font-semibold">
-              상태 :
-              {item.isAccepted}
-            </div>
+      {mission
+            && mission.map((item) => (
+              <ListCard key={item.missionId} className="grid grid-cols-6">
+                <FirstBox>
+                  <div className="text-lg font-semibold">
+                    상태 :
+                    {item.isAccepted}
+                  </div>
 
-            <Info>
-              제출일 :
-              {item.submitAt}
-            </Info>
-            <Info>
-              유저 ID :
-              {item.userId}
-            </Info>
-          </FirstBox>
-          <ThirdBox className="col-end-7">
-            <Button id={item.missionId} onClick={onUpdateHandler}>미션 승인</Button>
-          </ThirdBox>
-          <SecondBox className="col-span-5">
-            <Info>
-              url :
-              {' '}
-              {item.imageUrl}
-            </Info>
-          </SecondBox>
-
-        </ListCard>
-      ))}
+                  <Info>
+                    제출일 :
+                    {item.submitAt}
+                  </Info>
+                  <Info>
+                    유저 ID :
+                    {item.userId}
+                  </Info>
+                </FirstBox>
+                <ThirdBox className="col-end-7">
+                  {item.isAccepted === 'WAITING' && (
+                  <Button id={item.missionId} onClick={onUpdateHandler}>
+                    미션 승인
+                  </Button>
+                  )}
+                  {item.isAccepted === 'ACCEPTED' && (
+                  <Button id={item.missionId} onClick={onRejectHandler}>
+                    미션 승인 취소
+                  </Button>
+                  )}
+                </ThirdBox>
+                <SecondBox className="col-span-5">
+                  <Info>
+                    url :
+                    {' '}
+                    {item.imageUrl}
+                  </Info>
+                </SecondBox>
+              </ListCard>
+            ))}
     </ListContainer>
-
   );
 }
+
 const ListContainer = tw.div`
   bg-gray-200 p-4 rounded-lg
-    `;
+`;
 const ListCard = tw.div`
   bg-white p-4 my-4 rounded-lg shadow-md
 `;
 const Info = tw.div`
-text-gray-700
+  text-gray-700
 `;
 const FirstBox = tw.div`
-col-span-3
+  col-span-3
 `;
 const SecondBox = tw.div`
-col-span-2
+  col-span-2
 `;
 const ThirdBox = tw.div`
-col-span-1 flex justify-center items-center
+  col-span-1 flex justify-center items-center
 `;
 const Button = tw.button`
-    text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2
+    text-white bg-gray-800 hover:bg-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2
 `;
 export default MissionTab;
--- src/components/private/profilePage/MyInfo.jsx
+++ src/components/private/profilePage/MyInfo.jsx
@@ -169,6 +169,6 @@ const BoundInput = tw.input`
     w-full pl-2.5 pr-3 py-2 rounded-lg border-2 border-gray-200 outline-none
         `;
 const Button = tw.button`
-    text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2
+    text-white bg-gray-800 hover:bg-gray-900 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2
 `;
 export default MyInfo;
